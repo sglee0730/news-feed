631f3ff9dc28ba58fe8a2e0873d2d40852c649cd
unknown <stive2007@naver.com>
2024-02-26 19:55:31 +0900

    feat: add page service

---
+++ src/services/page.js
@@ -0,0 +1,85 @@
+import { Page, School, User } from '../models/index.js';
+
+export const getPage = async (schoolId) => {
+  const isSchoolExist = await School.exists({ id: schoolId });
+  if (!isSchoolExist) {
+    return { status: 404, message: '학교를 찾을 수 없습니다.' };
+  }
+
+  const page = await Page.findOne({ school: schoolId }).lean();
+  if (!page) {
+    return { status: 404, message: '학교 페이지가 존재하지 않습니다.' };
+  }
+
+  return page;
+};
+export const createPage = async (body, schoolId, userId) => {
+  const isSchoolExist = await School.exists({ id: schoolId });
+  if (!isSchoolExist) {
+    return { status: 404, message: '학교를 찾을 수 없습니다.' };
+  }
+
+  const isPageExist = await Page.exists({ school: schoolId }).lean();
+  if (isPageExist) {
+    return { status: 409, message: '학교 페이지가 이미 존재합니다.' };
+  }
+
+  const user = await User.findOne({ id: userId, role: 'Admin' }).lean();
+  if (!user) {
+    return { status: 403, message: '해당 작업을 수행할 권한이 없습니다.' };
+  }
+
+  const page = await Page.create({
+    createdBy: userId,
+    updatedBy: userId,
+    school: schoolId,
+    ...body,
+  });
+
+  return page;
+};
+export const updatePage = async (body, schoolId, userId) => {
+  const user = await User.findOne({ id: userId, role: 'Admin' }).lean();
+  if (!user) {
+    return { status: 403, message: '해당 작업을 수행할 권한이 없습니다.' };
+  }
+
+  const isSchoolExist = await School.exists({ id: schoolId });
+  if (!isSchoolExist) {
+    return { status: 404, message: '학교를 찾을 수 없습니다.' };
+  }
+
+  const isPageExist = await Page.exists({ school: schoolId }).lean();
+  if (!isPageExist) {
+    return { status: 404, message: '학교 페이지를 찾을 수 없습니다.' };
+  }
+
+  const page = await Page.updateOne(
+    { school: schoolId },
+    {
+      updatedBy: userId,
+      ...body,
+    },
+  );
+
+  return page;
+};
+
+export const deletePage = async (schoolId, userId) => {
+  const user = await User.findOne({ id: userId, role: 'Admin' }).lean();
+  if (!user) {
+    return { status: 403, message: '해당 작업을 수행할 권한이 없습니다.' };
+  }
+
+  const isSchoolExist = await School.exists({ id: schoolId });
+  if (!isSchoolExist) {
+    return { status: 404, message: '학교를 찾을 수 없습니다.' };
+  }
+
+  const isPageExist = await Page.exists({ school: schoolId }).lean();
+  if (!isPageExist) {
+    return { status: 404, message: '학교 페이지를 찾을 수 없습니다.' };
+  }
+
+  await Page.deleteOne({ school: schoolId });
+};
